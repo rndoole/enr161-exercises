60be375e03c0bcf11421249ff08b1642fd09f74c
rndoole <54540621+rndoole@users.noreply.github.com>
2019-10-11 09:08:17 -0400

    Update Blink.ino

--- Chapter_1&2/Blink/Blink.ino
+++ Chapter_1&2/Blink/Blink.ino
@@ -4,23 +4,23 @@
  * library variable LED_BUILTIN. Don't forget to update comments as
  * necessary.
  *
- * Author: 
- * Assisted by: 
- * Date: 
+ * Author: Ricardo Ndoole
+ * Assisted by: Peter
+ * Date: 10/10/2019
  */
 
-char led = 13;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin "led" as an output.
-  pinMode(led, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
 }
